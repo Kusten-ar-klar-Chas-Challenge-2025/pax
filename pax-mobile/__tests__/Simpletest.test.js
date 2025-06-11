5123e3c683913af66391f417c367c302425202e9
Hannakind <Hanna.kindholm@chasacademy.se>
2025-06-11 10:06:29 +0200

    test favoriteButton

--- pax-mobile/__tests__/Simpletest.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
-import { Text } from 'react-native';
-
-test('visar text korrekt', () => {
-    const { getByText } = render(<Text>Hello world</Text>);
-    expect(getByText('Hello world')).toBeTruthy();
-});
